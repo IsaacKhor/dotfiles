27d1ad767fdc90ac96ca454704b7c602cf928cf4
Isaac Khor <services@isaackhor.com>
2017-07-29 13:46:16 +0800

    Fix date format for fish_right_prompt

--- fish/config.fish
+++ fish/config.fish
@@ -6,4 +6,4 @@ set fish_user_paths "/usr/local/sbin" $fish_user_paths
 # bobthefish config options
 
 set theme_color_scheme solarized-dark
-set theme_date_format "+%a %H:%M"
+set theme_date_format "+%H:%M"
