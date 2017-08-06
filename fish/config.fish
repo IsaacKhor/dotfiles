27f5fb32124b8cd183b972e3d759c78261ed9274
Isaac Khor <s.github@isaackhor.com>
2017-08-06 15:53:17 +0800

    Install fisherman and change theme to solarized-light

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,7 @@ set fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 # bobthefish config options
 
-set theme_color_scheme solarized-dark
+set theme_color_scheme solarized-light
 set theme_date_format "+%H:%M"
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
