22cab4a11e88487337746622db5fa331798b6223
Isaac Khor <s.github@isaackhor.com>
2017-08-23 00:23:04 +0800

    Modify brew script to install java before other packages

--- macos/brew-install.sh
+++ macos/brew-install.sh
@@ -12,8 +12,11 @@ brew upgrade
 
 # Install all the brew packages
 
-brew install lua scala leiningen testdisk boot-clj brew-graph git-flow wget dnscrypt-proxy hugo fish rsync coreutils findutils ffmpeg
-brew cask install 1password hammerspoon monolingual tunnelblick android-platform-tools java musescore vlc caffeine launchbar transmission the-unarchiver suspicious-package app-cleaner firefox karabiner-elements macdown veracrypt google-chrome
+brew cask install java # Some formulae have java as a dip
+
+brew install lua scala leiningen testdisk boot-clj git-flow wget dnscrypt-proxy hugo fish rsync coreutils findutils ffmpeg
+
+brew cask install 1password hammerspoon monolingual tunnelblick android-platform-tools musescore vlc caffeine launchbar transmission the-unarchiver suspicious-package app-cleaner firefox karabiner-elements macdown veracrypt google-chrome iterm2
 
 brew cleanup
 brew cask cleanup
