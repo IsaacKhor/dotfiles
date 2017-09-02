c50d3fd67816c723bb71ae94240e588270258702
Isaac Khor <s.github@isaackhor.com>
2017-09-02 18:10:11 +0800

    Change hammerspoon tab switching to use default OS shortcut

--- .hammerspoon/ff-tab-switch.lua
+++ .hammerspoon/ff-tab-switch.lua
@@ -1,40 +1,10 @@
 local mod = {}
 mod.config = {
-  specialApps =
-  {["Firefox"] = true,
-   ["Sublime Text"] = true}
 }
 
---
--- Keybindings
---
-
--- Change the keybinding for special apps that don't deal with the native
--- system controls
-
-function frontmostApp()
-    return hs.application.frontmostApplication()
-end
-
--- Poor man's version of (contains? collection element)
--- by storing the names in the keys
-function isSpecial(appName)
-  return mod.config.specialApps[appName] ~= nil
-end
-
-function changeIfSpecial(orig, change)
-  if(isSpecial(frontmostApp():title())) then
-    emitKey(change[1], change[2])
-  else
-    emitKey(orig[1], orig[2])
-  end
-end
-
 function mod.init()
-  hs.hotkey.bind("ctrl", "s",
-    function() changeIfSpecial({"ctrl,alt,shift,cmd", "f1"}, {"ctrl", "tab"}) end)
-  hs.hotkey.bind("ctrl", "n",
-    function() changeIfSpecial({"ctrl,alt,shift,cmd", "f2"}, {"ctrl,shift", "tab"}) end)
+  hs.hotkey.bind("ctrl", "s", function() emitKey("cmd,shift", "]") end)
+  hs.hotkey.bind("ctrl", "n", function() emitKey("cmd,shift", "[") end)
 end
 
 return mod
