b366f4fa2f95a1c6826bf95695b9622004a58e66
Isaac Khor <s.github@isaackhor.com>
2017-09-02 18:10:40 +0800

    Limit boot memory usage

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 # Set up environment
 
 set EDITOR vim
+set BOOT_JVM_OPTIONS '-XX:MaxMetaspaceSize=128m -Xms256m -Xmx512m -client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none'
 set fish_user_paths "/usr/local/sbin" "/usr/local/opt/coreutils/libexec/gnubin" $fish_user_paths
 
 # bobthefish config options
