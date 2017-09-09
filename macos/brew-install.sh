5c2bd9926a0ae763d353ecdacf1ef6f935461cef
Isaac Khor <s.github@isaackhor.com>
2017-09-09 14:02:02 +0800

    Add emacs and fonts to brew

--- macos/brew-install.sh
+++ macos/brew-install.sh
@@ -8,12 +8,28 @@ brew update
 brew upgrade
 
 # Install homebrew-cask
-#brew tap caskroom/cask # Latest versions of homebrew comes with cask
+brew tap caskroom/fonts
 
 # Install all the brew packages
 
-brew install lua scala leiningen testdisk boot-clj brew-graph git-flow wget dnscrypt-proxy hugo fish rsync coreutils findutils ffmpeg
-brew cask install 1password hammerspoon monolingual tunnelblick android-platform-tools java musescore vlc caffeine launchbar transmission the-unarchiver suspicious-package app-cleaner firefox karabiner-elements macdown veracrypt google-chrome
+# Normal formulae
+brew install lua scala leiningen testdisk boot-clj brew-graph git-flow wget
+brew install dnscrypt-proxy hugo fish rsync coreutils findutils ffmpeg
 
-brew cleanup
+# Casks
+brew cask install 1password hammerspoon monolingual tunnelblick
+brew cask install android-platform-tools java musescore vlc caffeine launchbar
+brew cask install transmission the-unarchiver suspicious-package app-cleaner
+brew cask install firefox karabiner-elements macdown veracrypt google-chrome
+brew cask install emacs
+
+# Fonts
+brew cask install font-eb-garamond font-fira-code font-hasklig
+brew cask install font-inconsolata font-lora font-open-sans
+brew cask install font-roboto font-roboto-mono
+brew cask install font-source-code-pro-for-powerline
+brew cask install font-ubuntu
+
+# Cleanup
+brew cleanup -s
 brew cask cleanup
