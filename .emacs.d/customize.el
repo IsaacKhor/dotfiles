218fa187274db23956090ccc4e46f59db3b61d1b
Isaac Khor <s.github@isaackhor.com>
2017-09-10 16:02:55 +0800

    Change init.el to use use-package to configure packages and change kill keybindings

--- .emacs.d/customize.el
+++ .emacs.d/customize.el
@@ -3,12 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d/autosaves/"))))
  '(bs-configurations
    (quote
     (("all" nil nil nil nil nil)
      ("files-and-scratch" "^\\*scratch\\*$" nil nil bs-visits-non-file bs-sort-buffer-interns-are-last))))
  '(bs-cycle-configuration-name nil)
- '(bs-default-configuration "all")
+ '(bs-default-configuration "files-and-scratch")
  '(bs-default-sort-name "by name")
  '(column-number-mode t)
  '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
@@ -16,7 +17,7 @@
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
  '(debug-on-error t)
- '(default-frame-alist (quote ((height . 60) (width . 115))))
+ '(default-frame-alist (quote ((height . 55) (width . 120))))
  '(electric-pair-mode t)
  '(exec-path
    (quote
@@ -44,7 +45,7 @@
      ("melpa" . "https://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (geiser dired+ dim company smart-mode-line slime helm-projectile bind-key rainbow-delimiters rainbow-mode helm helm-cider fill-column-indicator all-the-icons neotree lacarte icicles magit markdown-mode markdown-mode+ lua-mode brainfuck-mode org parinfer color-theme-sanityinc-solarized color-theme cider)))
+    (use-package geiser dired+ dim company smart-mode-line slime helm-projectile bind-key rainbow-delimiters rainbow-mode helm helm-cider fill-column-indicator all-the-icons neotree lacarte icicles magit markdown-mode markdown-mode+ lua-mode brainfuck-mode org parinfer color-theme-sanityinc-solarized color-theme cider)))
  '(projectile-mode t nil (projectile))
  '(recentf-max-menu-items 100)
  '(recentf-max-saved-items 100)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,83 +16,18 @@
 (add-hook 'kill-emacs-query-functions
           'custom-prompt-customize-unsaved-options)
 
-;; ===========
-;; MINOR MODES
-;; ===========
-
-; Minor modes
-(defun add-hook-to-minor-mode (mode hooks)
+(defun add-minor-mode-to-hooks (mode hooks)
   (dolist (hook hooks)
     (add-hook hook mode)))
-
-; Parinfer for all the lisps
-(add-hook-to-minor-mode #'parinfer-mode
-   '(clojure-mode-hook
-     emacs-lisp-mode-hook
-     ielm-mode-hook
-     lisp-mode-hook
-     lisp-interaction-mode-hook
-     scheme-mode-hook
-     eval-expression-minibuffer-setup-hook))
 
 ; Fix auto indentation in some modes
 (defun fix-indent ()
   (setq electric-indent-inhibit t))
 
-(add-hook-to-minor-mode #'fix-indent
+(add-minor-mode-to-hooks #'fix-indent
    '(emacs-lisp-mode-hook
      lisp-interaction-mode-hook
      lisp-mode-hook))
-
-; Add fill column indicator (fci) for certain modes
-(add-hook-to-minor-mode #'fci-mode
-  '(emacs-lisp-mode-hook
-    lisp-mode-hook
-    lisp-interaction-mode-hook
-    clojure-mode-hook))
-
-; CIDER for Clojure(Script)
-(add-hook 'clojure-mode-hook #'cider-mode)
-
-; Autocomplete via company-mode
-(global-company-mode)
-
-; Disable FCI while company presents popup; workaround described
-; here: https://github.com/company-mode/company-mode/issues/180
-; Or else fci will interfere with the popup
-(defvar-local company-fci-mode-on-p nil)
-
-(defun company-turn-off-fci (&rest ignore)
-  (when (boundp 'fci-mode)
-    (setq company-fci-mode-on-p fci-mode)
-    (when fci-mode (fci-mode -1))))
-
-(defun company-maybe-turn-on-fci (&rest ignore)
-  (when company-fci-mode-on-p (fci-mode 1)))
-
-(add-hook 'company-completion-started-hook 'company-turn-off-fci)
-(add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
-(add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci)
-
-; Longlines mode for text
-; Change nav behaviour when dealing with text
-(add-hook 'text-mode (lambda () (longlines-mode 1)))
-
-; Smart mode line
-(sml/setup)
-
-; Change mode names
-(dim-major-names
- '((emacs-lisp-mode "ELisp")
-   (lisp-interaction-mode "LispInt")))
-
-(dim-minor-names
-  '((helm-mode "")
-    (company-mode "")
-    (visual-line-mode "")))
-
-; Custom parinfer mode display names
-(setq parinfer-lighters '(" (i)" . " (p)"))
 
 ; Icicles don't want to work with Customize for some reason, so
 ; we enable it manually
@@ -104,51 +39,126 @@
   (lambda () (rainbow-delimiters-mode 1)))
 (global-rainbow-delimiters-mode 1)
 
-; The default electrical auto indent is stupid
-; It randomly indents away all of your comments, especially in elisp
-(setq-default electric-indent-inhibit t)
-
-;; ===================
-;; HELM AND EXTENSIONS
-;; ===================
-
-; Helm configuration
-(require 'helm-config)
-(bind-key "C-c h" 'helm-command-prefix)
-(unbind-key "C-x c")
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-(define-key helm-map (kbd "C-z") 'helm-select-action)
-(setq
-  ; open helm buffer inside current window, not occupy whole other window
-  helm-split-window-in-side-p           t
-  ; move to end or beginning of source when reaching top or bottom of source.
-  helm-move-to-line-cycle-in-source     t
-  ; search for library in `require' and `declare-function' sexp.
-  helm-ff-search-library-in-sexp        t
-  ; scroll 8 lines other window using M-<next>/M-<prior>
-  helm-scroll-amount                    8
-  helm-ff-file-name-history-use-recentf t
-  helm-echo-input-in-header-line t)
-(helm-mode 1)
-
-; Helm projectile
-(projectile-global-mode)
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
-
 ;; ===========
 ;; Keybindings
 ;; ===========
-(bind-key "C-x C-r" #'helm-recentf)
-(bind-key "<f8>" #'neotree-toggle)
 (bind-key "C-x k" #'kill-this-buffer)
-(bind-key "C-x C-b" #'helm-mini)
-(bind-key "C-{" #'bs-cycle-next) 
-(bind-key "C-}" #'bs-cycle-previous)
-(bind-key "M-x" #'helm-M-x)
-(bind-key "C-x b" #'bs-show)
-(bind-key "C-x C-f" #'helm-find-files)
-(bind-key "C-c m" #'magit-status)
-(bind-key "C-'" #'helm-projectile-find-file)
-(bind-key "C-," #'helm-mini)
-(bind-key "<tab>" #'company-indent-or-complete-common)
+(bind-key "C-c <f1> v" #'customize-variable)
+(bind-key "C-k" #'kill-region)
+(bind-key "M-k" #'kill-ring-save)
+
+; Company
+(use-package company
+  :bind
+  (("<tab>" . company-indent-or-complete-common) 
+   :map company-active-map
+   ("<up>"   . company-select-previous)
+   ("<down>" . company-select-next)
+   ("C-p" . comany-select-previous)
+   ("C-n" . company-select-next)
+   ("C-d" . company-show-doc-buffer)
+   ("M-." . company-show-location))
+
+  :config
+  (global-company-mode))
+
+; Helm
+(use-package helm
+  :init
+  (require 'helm-config)
+  (bind-key "C-c h" 'helm-command-prefix)
+  (unbind-key "C-x c")
+  (setq helm-split-window-in-side-p t
+        helm-move-to-line-cycle-in-source t
+        helm-ff-search-library-in-sexp t
+        helm-scroll-amount 8
+        helm-echo-input-in-header-line t
+        projectile-completion-system 'helm)
+
+  :bind
+  (("M-x"     . helm-M-x)
+   ("C-x C-r" . helm-recentf)
+   ("C-x C-b" . helm-mini)
+   ("C-x C-," . helm-mini)
+   ("C-x C-f" . helm-find-files)
+   ("C-'"     . helm-projectile-find-file)
+
+   :map helm-map
+   ("<tab>" . helm-execute-persistent-action)
+   ("C-z"   . helm-select-action))
+
+  :config
+  (helm-mode 1)
+  (projectile-global-mode)
+  (helm-projectile-on))
+
+(use-package fill-column-indicator
+  :config
+  (add-minor-mode-to-hooks #'fci-mode
+    '(emacs-lisp-mode-hook
+      lisp-mode-hook
+      lisp-interaction-mode-hook
+      clojure-mode-hook))
+  
+  ; Disable FCI while company presents popup; workaround described
+  ; here: https://github.com/company-mode/company-mode/issues/180
+  ; Or else fci will interfere with the popup
+  (defvar-local company-fci-mode-on-p nil)
+  (defun company-turn-off-fci (&rest ignore)
+    (when (boundp 'fci-mode)
+      (setq company-fci-mode-on-p fci-mode)
+      (when fci-mode (fci-mode -1))))
+  (defun company-maybe-turn-on-fci (&rest ignore)
+    (when company-fci-mode-on-p (fci-mode 1)))
+
+  (add-hook 'company-completion-started-hook 'company-turn-off-fci)
+  (add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
+  (add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci))
+
+(use-package parinfer
+  :config
+  (add-minor-mode-to-hooks #'parinfer-mode
+   '(clojure-mode-hook
+     emacs-lisp-mode-hook
+     ielm-mode-hook
+     lisp-mode-hook
+     lisp-interaction-mode-hook
+     scheme-mode-hook
+     eval-expression-minibuffer-setup-hook))
+  (setq parinfer-lighters '(" (i)" . " (p)")))
+
+(use-package cider
+  :config
+  (add-hook 'clojure-mode-hook #'cider-mode))
+
+(use-package smart-mode-line
+  :init
+  (setq sml/theme 'dark)
+  
+  :config
+  (sml/setup))
+
+(use-package dim
+  :config
+  (dim-major-names
+   '((emacs-lisp-mode "ELisp")
+     (lisp-interaction-mode "LispInt")))
+
+  (dim-minor-names
+    '((helm-mode "")
+      (company-mode "")
+      (visual-line-mode ""))))
+
+(use-package magit
+  :bind
+  (("C-c m" . magit-status)))
+
+(use-package neotree
+  :bind
+  (("<f8>" . neotree-toggle)))
+
+(use-package bs
+  :bind
+  (("C-{" . bs-cycle-previous)
+   ("C-}" . bs-cycle-next)
+   ("C-x b" . bs-show)))
