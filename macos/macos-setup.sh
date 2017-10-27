c228ba8470bcc1a1e4879546a37a4fd99e0fc18f
Isaac Khor <s.github@isaackhor.com>
2017-10-27 18:25:07 +0800

    ?

--- macos/macos-setup.sh
+++ macos/macos-setup.sh
@@ -23,8 +23,6 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 . ./settings-system.sh
 . ./settings-term.sh
 
-. ./brew-install.sh
-
 
 ###############################################################################
 # Kill affected applications                                                  #
