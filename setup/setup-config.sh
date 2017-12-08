ae76378d6aa6c061409423391dd113095408db8d
Isaac Khor <s.github@isaackhor.com>
2017-12-08 21:28:36 +0800

    Change symlinks to use general ln dir/* dir

--- setup/setup-config.sh
+++ setup/setup-config.sh
@@ -1,80 +1,23 @@
 #!/bin/bash
 
-# Initial setup
-ln -sf /Users/home/.config  ~/.config
-ln -sf /Users/home/.misc    ~/.misc
-ln -lf /Users/home          ~/home
-ln -sf ~/.misc/.local       ~/.local
+# Symlink everything
 
-# HOME dir
-ln -sf ~/.config/Home/.iterm2_shell_integration.fish ~/.iterm2_shell_integration.fish
-ln -sf ~/.config/Home/.atom         ~/.atom
-ln -sf ~/.config/Home/.gnupg        ~/.gnupg
-ln -sf ~/.config/Home/.iterm2       ~/.iterm2
-ln -sf ~/.config/Home/.ssh          ~/.ssh
-ln -sf ~/.config/Home/bin           ~/bin
-ln -sf ~/.config/Home/quicklisp     ~/quicklisp
-ln -sf ~/.config/Home/.gitconfig    ~/.gitconfig
-ln -sf ~/.config/Home/.sbclrc       ~/.sbclrc
-ln -sf ~/.config/Home/.viminfo      ~/.viminfo
-ln -sf ~/.config/Home/.gem          ~/.gem
-ln -sf ~/.config/Home/.anaconda     ~/.anaconda
-ln -sf ~/.config/Home/.bundle       ~/.bundle
-#ln -sf ~/.config/Home/ ~/
+# Dotfiles
+ln -sfFn ~/Documents/dotfiles/scripts/ ~/scripts
+ln -sfFn ~/Documents/dotfiles/.wgetrc ~/.wgetrc
+ln -sfFn ~/Documents/dotfiles/karabiner ~/.config/karabiner
+ln -sfFn ~/Documents/dotfiles/fish ~/.config/fish
+ln -sfFn ~/Documents/dotfiles/.hammerspoon ~/.hammerspoon
+ln -sfFn ~/Documents/dotfiles/.emacs.d/ ~/.emacs.d/
 
-# dotfiles
-ln -sf ~/home/dotfiles/.hammerspoon ~/.hammerspoon
+# Home dir
+ln -sfFn ~/Documents/.hidden/Home/* ~/
+ln -sfFn ~/Documents/.hidden/.config ~/.config
+ln -sfFn ~/Documents/.hidden/localshare ~/.local/share
 
-# Within ~/Library itself
-ln -sf ~/.config/Library/Fonts          ~/Library/Fonts
-ln -sf ~/.config/Library/Keychains      ~/Library/Keychains
+ln -sfFn ~/Documents/.hidden/Google\ Drive/* ~/Documents
 
-# Firefox
-mkdir -p ~/Library/Application\ Support/Firefox/Profiles
-ln -sf ~/.config/Library/Custom/profiles.ini        ~/Library/Application\ Support/Firefox/profiles.ini
-ln -sf ~/.config/Library/Custom/profile.default     ~/Library/Application\ Support/Firefox/Profiles/profile.default
-
-# Application Support
-ln -sf ~/.config/Library/Application\ Support/1Password\ 4             ~/Library/Application\ Support/1Password\ 4
-ln -sf ~/.config/Library/Application\ Support/AndroidStudio3.0         ~/Library/Application\ Support/AndroidStudio3.0
-ln -sf ~/.config/Library/Application\ Support/Atom                     ~/Library/Application\ Support/Atom
-ln -sf ~/.config/Library/Application\ Support/IntelliJIdea2017.2       ~/Library/Application\ Support/IntelliJIdea2017.2
-ln -sf ~/.config/Library/Application\ Support/LaunchBar                ~/Library/Application\ Support/LaunchBar
-ln -sf ~/.config/Library/Application\ Support/LightTable               ~/Library/Application\ Support/LightTable
-ln -sf ~/.config/Library/Application\ Support/MacDown                  ~/Library/Application\ Support/MacDown
-ln -sf ~/.config/Library/Application\ Support/Sublime\ Text\ 3         ~/Library/Application\ Support/Sublime\ Text\ 3
-ln -sf ~/.config/Library/Application\ Support/VeraCrypt                ~/Library/Application\ Support/VeraCrypt
-ln -sf ~/.config/Library/Application\ Support/com.operasoftware.Opera  ~/Library/Application\ Support/com.operasoftware.Opera
-#ln -sf ~/.config/Library/Application\ Support/      ~/Library/Application\ Support/
-
-# Containers
-ln -sf ~/.config/Library/Containers/com.apple.calculator           ~/Library/Containers/com.apple.calculator
-ln -sf ~/.config/Library/Containers/com.apple.iWork.Keynote        ~/Library/Containers/com.apple.iWork.Keynote
-ln -sf ~/.config/Library/Containers/com.apple.iWork.Numbers        ~/Library/Containers/com.apple.iWork.Numbers
-ln -sf ~/.config/Library/Containers/com.apple.iWork.Pages          ~/Library/Containers/com.apple.iWork.Pages
-ln -sf ~/.config/Library/Containers/com.apple.TextEdit             ~/Library/Containers/com.apple.TextEdit
-ln -sf ~/.config/Library/Containers/com.omnigroup.OmniFocus2       ~/Library/Containers/com.omnigroup.OmniFocus2
-ln -sf ~/.config/Library/Containers/com.omnigroup.OmniOutliner5    ~/Library/Containers/com.omnigroup.OmniOutliner5
-ln -sf ~/.config/Library/Containers/com.ulyssesapp.mac             ~/Library/Containers/com.ulyssesapp.mac
-#ln -sf ~/.config/Library/Containers/           ~/Library/Containers/
-
-# Preferences
-ln -sf ~/.config/Library/Preferences/AndroidStudio3.0                     ~/Library/Preferences/AndroidStudio3.0
-ln -sf ~/.config/Library/Preferences/IntelliJIdea2017.2                   ~/Library/Preferences/IntelliJIdea2017.2
-ln -sf ~/.config/Library/Preferences/at.obdev.LaunchBar.plist             ~/Library/Preferences/at.obdev.LaunchBar.plist
-ln -sf ~/.config/Library/Preferences/co.gitup.mac.plist                   ~/Library/Preferences/co.gitup.mac.plist
-ln -sf ~/.config/Library/Preferences/com.agilebits.onepassword4.plist     ~/Library/Preferences/com.agilebits.onepassword4.plist
-ln -sf ~/.config/Library/Preferences/com.github.atom.plist                ~/Library/Preferences/com.github.atom.plist
-ln -sf ~/.config/Library/Preferences/com.google.Chrome.plist              ~/Library/Preferences/com.google.Chrome.plist
-ln -sf ~/.config/Library/Preferences/com.google.GoogleDrive.plist         ~/Library/Preferences/com.google.GoogleDrive.plist
-ln -sf ~/.config/Library/Preferences/com.googlecode.iterm2.plist          ~/Library/Preferences/com.googlecode.iterm2.plist
-ln -sf ~/.config/Library/Preferences/com.jetbrains.intellij.plist         ~/Library/Preferences/com.jetbrains.intellij.plist
-ln -sf ~/.config/Library/Preferences/com.kodowa.LightTable.plist          ~/Library/Preferences/com.kodowa.LightTable.plist
-ln -sf ~/.config/Library/Preferences/com.omnigroup.OmniOutliner5.plist    ~/Library/Preferences/com.omnigroup.OmniOutliner5.plist
-ln -sf ~/.config/Library/Preferences/com.sublimetext.3.plist              ~/Library/Preferences/com.sublimetext.3.plist
-ln -sf ~/.config/Library/Preferences/com.ulyssesapp.mac.plist             ~/Library/Preferences/com.ulyssesapp.mac.plist
-ln -sf ~/.config/Library/Preferences/com.uranusjr.macdown.plist           ~/Library/Preferences/com.uranusjr.macdown.plist
-ln -sf ~/.config/Library/Preferences/net.freemacsoft.AppCleaner.plist     ~/Library/Preferences/net.freemacsoft.AppCleaner.plist
-ln -sf ~/.config/Library/Preferences/org.hammerspoon.Hammerspoon.plist    ~/Library/Preferences/org.hammerspoon.Hammerspoon.plist
-ln -sf ~/.config/Library/Preferences/org.mozilla.firefox.plist            ~/Library/Preferences/org.mozilla.firefox.plist
-ln -sf ~/.config/Library/Preferences/org.torproject.torbrowser.plist      ~/Library/Preferences/org.torproject.torbrowser.plist
+# Library
+ln -sfFn ~/Documents/.hidden/Library/Application\ Support/* ~/Library/Application\ Support/
+ln -sfFn ~/Documents/.hidden/Library/Container/* ~/Library/Container/
+ln -sfFn ~/Documents/.hidden/Library/Preferences/* ~/Library/Preferences/
