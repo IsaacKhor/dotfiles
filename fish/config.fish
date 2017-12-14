06ff097a79944ea407080acac0b7b6892e26a6c7
Isaac Khor <s.github@isaackhor.com>
2017-12-14 23:53:08 +0800

    fish mods

--- fish/config.fish
+++ fish/config.fish
@@ -15,16 +15,8 @@ set theme_display_docker_machine yes
 set theme_display_hg yes
 set theme_display_virtualenv yes
 set theme_display_ruby yes
-# set theme_display_user yes
-set theme_display_vi yes
-# set theme_avoid_ambiguous_glyphs yes
 set theme_powerline_fonts yes
-# set theme_nerd_fonts yes
 set theme_show_exit_status yes
-# set default_user your_normal_user
-# set theme_color_scheme dark
-# set fish_prompt_pwd_dir_length 0
-# set theme_project_dir_length 1
 set theme_newline_cursor yes
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
