1db5485bc074b300d59994feb4543c3122e8ec37
Isaac Khor <s.github@isaackhor.com>
2017-12-17 22:13:41 +0800

    Remove .emacs from tracking

--- .emacs.d/elpa/dim-20160818.249/dim-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dim" "20160818.249" "Change mode-line names of major/minor modes" '((emacs "24.4")) :commit "4b00587dfaabc1f2393b9a9f9993996c288d4445" :url "https://github.com/alezost/dim.el" :keywords '("convenience"))
--- .emacs.d/elpa/doremi-cmd-20170221.1631/doremi-cmd-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "doremi-cmd" "20170221.1631" "Miscellaneous Do Re Mi commands" '((doremi "0")) :url "https://www.emacswiki.org/emacs/download/doremi-cmd.el" :keywords '("keys" "cycle" "repeat"))
