db3ebe6ccbd5065e54281bdefbe9bd70d00ef80b
Isaac Khor <s.github@isaackhor.com>
2017-12-19 17:03:44 +0800

    Misc changes

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -22,76 +22,76 @@ hs.alert.defaultStyle.radius = 12
 -- Plugin config and loading
 
 local plugins = {
-    {"util.config-reload", {}},
+  {"util.config-reload", {}},
 
-    -- Utilities
-    {"util.magic-remapping", {}},
-    {"util.cheatsheet", {}},
-    {"util.window-hints", {}},
+  -- Utilities
+  {"util.magic-remapping", {}},
+  {"util.cheatsheet", {}},
+  {"util.window-hints", {}},
 }
 
 local function loadPlugin(plugin, config)
-    local mod = require(plugin)
+  local mod = require(plugin)
 
-    -- Proper modules should return tables
-    if type(mod) == "table" then
-        -- Create config if one doesn't exist
-        if not mod.config then
-            mod.config = {}
-        end
-
-        -- Config can be nil
-        if not config then
-            config = {}
-        end
-
-        -- Override default config
-        for k,v in pairs(config) do
-            mod.config[k] = v
-        end
-
-        -- Call init if it exists
-        -- Initialiser should be in a special function because
-        -- we might change config which will affect initialisation
-        if type(mod.init) == "function" then
-            mod.init()
-        end
+  -- Proper modules should return tables
+  if type(mod) == "table" then
+    -- Create config if one doesn't exist
+    if not mod.config then
+      mod.config = {}
     end
+
+    -- Config can be nil
+    if not config then
+      config = {}
+    end
+
+    -- Override default config
+    for k, v in pairs(config) do
+      mod.config[k] = v
+    end
+
+    -- Call init if it exists
+    -- Initialiser should be in a special function because
+    -- we might change config which will affect initialisation
+    if type(mod.init) == "function" then
+      mod.init()
+    end
+  end
 end
 
 local function loadPlugins(plugins)
-    for i = 1, #plugins do
-        loadPlugin(plugins[i][1], plugins[1][2])
-    end
+  for i = 1, #plugins do
+    loadPlugin(plugins[i][1], plugins[1][2])
+  end
 end
 
 -- A bind that works with config
 function bind(key, func)
-    hs.hotkey.bind(key[1], key[2], func)
+  hs.hotkey.bind(key[1], key[2], func)
 end
 
 function notify(header, text)
-    hs.notify.new({title = header, informativeText = text}):send()
+  hs.notify.new({title = header, informativeText = text}):send()
 end
 
 function prettyPrint(obj)
-    print(hs.inspect.inspect(obj))
+  print(hs.inspect.inspect(obj))
 end
 
 function alert(msg)
-    hs.alert.show(msg)
+  hs.alert.show(msg)
 end
 
 function emitKey(mod, key)
-    hs.eventtap.keyStroke(mod, key, 1000)
+  hs.eventtap.keyStroke(mod, key, 1000)
 end
 
 table.filter = function(coll, f)
-    local out = {}
-    for k,v in pairs(coll) do
-        if f(v) then out[k]=v end
-    end
-    return out
+  local out = {}
+  for k, v in pairs(coll) do
+    if f(v) then out[k] = v end
+  end
+  return out
 end
 
 --
