3103abada03c26f0a918f41e4d1c02b7dffec99a
Isaac Khor <s.github@isaackhor.com>
2017-12-25 00:12:43 +0800

    Rearrange setup scripts

---
+++ scripts/setup-dotfiles.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ln -sfFn ../.hammerspoon/ ~/.hammerspoon
+ln -sfFn ../.proton ~/.proton
+ln -sfFn ../.wgetrc/ ~/.wgetrc
+ln -sfFn ../fish/ ~/.config/fish
+ln -sfFn ../karabiner/ ~/.config/karabiner
+ln -sfFn ../scripts/ ~/scripts
