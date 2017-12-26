ed39a6b8d8a045b90ba1a8d8a07c6288dd0547b8
Isaac Khor <s.github@isaackhor.com>
2017-12-26 23:02:31 +0800

    Add rustup and completions and nvim linking

--- scripts/setup-dotfiles.sh
+++ scripts/setup-dotfiles.sh
@@ -5,4 +5,5 @@ ln -sfFn ../.proton ~/.proton
 ln -sfFn ../.wgetrc/ ~/.wgetrc
 ln -sfFn ../fish/ ~/.config/fish
 ln -sfFn ../karabiner/ ~/.config/karabiner
+ln -sfFn ../nvim/ ~/.config/nvim
 ln -sfFn ../scripts/ ~/scripts
