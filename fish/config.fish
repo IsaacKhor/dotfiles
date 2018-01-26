d6ea4621fdca6978999f2a27d2b7f986276b4f3f
Isaac Khor <s.github@isaackhor.com>
2018-01-26 14:09:51 +0800

    Move path to be relative to HOME and delete bobthefish settings

--- fish/config.fish
+++ fish/config.fish
@@ -3,23 +3,9 @@
 set EDITOR vimr
 set BOOT_JVM_OPTIONS '-XX:MaxMetaspaceSize=128m -Xms256m -Xmx512m -client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none'
 set PATH "/usr/local/opt/coreutils/libexec/gnubin" $PATH
-set PATH "/Users/cclark/Documents/dotfiles/scripts" $PATH
-set PATH "/Users/cclark/.local/bin" $PATH
-set PATH "/Users/cclark/.cargo/bin" $PATH
-
-# bobthefish config options
-
-set theme_color_scheme solarized-light
-set theme_date_format "+%H:%M"
-set theme_display_git_ahead_verbose yes
-set theme_git_worktree_support yes
-set theme_display_vagrant yes
-set theme_display_docker_machine yes
-set theme_display_hg yes
-set theme_display_virtualenv yes
-set theme_display_ruby yes
-set theme_powerline_fonts yes
-set theme_show_exit_status yes
-set theme_newline_cursor yes
+set PATH ~/dotfiles/scripts $PATH
+set PATH ~/.local/bin $PATH
+set PATH ~/.cargo/bin $PATH
+set PATH ~/bin $PATH
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
