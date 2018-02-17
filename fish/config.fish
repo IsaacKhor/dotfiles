91665094c6b7b2b78b3f08002ea0943367dad463
Isaac Khor <s.github@isaackhor.com>
2018-02-17 16:59:10 +0800

    Add virtualfish

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,11 @@ set EDITOR vimr
 set BOOT_JVM_OPTIONS '-XX:MaxMetaspaceSize=128m -Xms256m -Xmx512m -client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none'
 set PATH "/usr/local/opt/coreutils/libexec/gnubin" $PATH
 set PATH ~/dotfiles/scripts $PATH
-# set PATH ~/.local/bin $PATH
-# set PATH ~/.cargo/bin $PATH
 set PATH ~/bin $PATH
 
+# Virtualfish - virtualenv integration
+eval (python3 -m virtualfish)
+set VIRTUALFISH_HOME ~/.config/virtualenv
+
+# iTerm2 integration
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
