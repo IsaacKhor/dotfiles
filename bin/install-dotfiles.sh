5c344a6951aab378544544fb1238aa7505e4ade3
Isaac Khor <s.github@isaackhor.com>
2022-09-30 12:10:45 -0400

    Fix git remote config and ssh

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -4,26 +4,15 @@ set -euo pipefail
 
 # Set up dotfiles
 git clone --bare https://github.com/IsaacKhor/dotfiles.git ~/.dotfiles
-
-function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
-}
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 
 mkdir -p .config-backup
-dotfiles checkout
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
-dotfiles checkout
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
 
-# Install and update misc utilities
-echo 'Configuring ZSH'
-curl -L git.io/antigen > ~/bin/antigen.zsh
-
-echo 'Configuring tmux'
-mkdir -p ~/.config/tmux/plugins
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
-~/.config/tmux/plugins/tpm/bin/install_plugins
