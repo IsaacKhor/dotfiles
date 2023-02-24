a28f5368118e6f0cd9ea652d2715b9051fb2aac6
Isaac Khor <s.github@isaackhor.com>
2023-02-24 23:05:20 +0000

    Change default shell to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,14 @@
-
 # Starship prompt
 starship init fish | source
+
+fish_add_path ~/.cargo/bin
+fish_add_path /home/linuxbrew/.linuxbrew/bin
+
+alias ll="exa -lh"
+alias la="exa -lah"
+alias dotfile="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+# opam configuration
+if test -e $HOME/.opam/opam-init/init.fish
+	source /home/isaackhor/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+end
