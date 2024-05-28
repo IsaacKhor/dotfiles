754d0e083a6ee8f0e684493bb7779c3553e0a330
Isaac Khor <s.github@isaackhor.com>
2024-05-28 10:35:05 -0400

    New config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,35 @@
-fish_add_path ~/.cargo/bin
-
 # Starship prompt
 if type -q starship
 	starship init fish | source
 end
 
-alias ls="eza"
-alias ll="eza -lh"
-alias la="eza -lah"
 alias dotfile="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
 # opam configuration
 if test -e $HOME/.opam/opam-init/init.fish
 	source /home/isaackhor/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 end
+
+if test -e /opt/homebrew
+	fish_add_path -g /opt/homebrew/bin
+end
+
+if test -e ~/.cargo
+	fish_add_path -g ~/.cargo/bin
+end
+
+if type -q starship
+	starship init fish | source
+end
+
+if type -q eza
+	alias ls="eza"
+	alias ll="eza -lh"
+	alias la="eza -lah"
+end
+
+# source machine local config if one exists
+if test -e ~/.config/fish/config.$hostname
+	source ~/.config/fish/config.$hostname
+end
+
