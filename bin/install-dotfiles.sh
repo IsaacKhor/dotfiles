a02219d0fde1d0e760a4449e90c73620b61f90c6
Isaac Khor <s.github@isaackhor.com>
2024-12-20 20:24:58 +0800

    new setup scripts

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -18,9 +18,17 @@ fi;
 
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote set-url origin git@github.com:IsaacKhor/dotfiles.git
 
-sudo apt -y install fish fzf build-essential cmake gpg
+sudo apt -y install fish fzf gpg
 
 # setup rust toolchain
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cargo install eza starship
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#cargo install eza starship
+
+curl -sS https://starship.rs/install.sh | sh
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
 
---
+++ bin/setup-machine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+
+adduser ikhor
+usermod -aG sudo ikhor
+echo 'ikhor ALL=NOPASSWD: ALL' > /etc/sudoers.d/ikhor_nopasswd
+
+curl https://raw.githubusercontent.com/IsaacKhor/dotfiles/refs/heads/master/bin/install-dotfiles.sh > /tmp/install_dotfiles.sh
+su -c 'bash /tmp/install_dotfiles.sh' - ikhor
+
