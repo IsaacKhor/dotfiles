8455d378d91326f63c74a49311a93107431a4d40
Isaac Khor <s.github@isaackhor.com>
2025-02-10 16:46:41 -0500

    make linux script more resilient

--- bin/linux_packages.bash
+++ bin/linux_packages.bash
@@ -9,6 +9,7 @@ sudo apt -y install fish fzf gpg
 
 curl -sS https://starship.rs/install.sh | sudo sh -s -- --yes
 sudo mkdir -p /etc/apt/keyrings
+rm -rf /etc/apt/keyrings/gierens.gpg
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
