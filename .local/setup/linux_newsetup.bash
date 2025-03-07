9097d7e3ffc73ff16b47cd01e2d34b894a1a188b
Isaac Khor <s.github@isaackhor.com>
2025-03-07 14:32:18 -0500

    fix setup scripts path

--- .local/setup/linux_newsetup.bash
+++ .local/setup/linux_newsetup.bash
@@ -10,6 +10,6 @@ deluser --remove-home ikhor || true
 adduser --disabled-password --gecos '' ikhor
 usermod -aG sudo ikhor
 echo 'ikhor ALL=NOPASSWD: ALL' > /etc/sudoers.d/ikhor_nopasswd
-curl https://raw.githubusercontent.com/IsaacKhor/dotfiles/refs/heads/master/bin/setup_dotfiles.bash | su -c 'bash' - ikhor
-curl https://raw.githubusercontent.com/IsaacKhor/dotfiles/refs/heads/master/bin/linux_packages.bash | su -c 'bash' - ikhor
+curl https://raw.githubusercontent.com/IsaacKhor/dotfiles/refs/heads/master/.local/setup/setup_dotfiles.bash | su -c 'bash' - ikhor
+curl https://raw.githubusercontent.com/IsaacKhor/dotfiles/refs/heads/master/.local/setup/linux_packages.bash | su -c 'bash' - ikhor
 
